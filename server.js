73824778f4c11a2eea550df9e8f9db3beacad240
David Marchante <cdmarchante@gmail.com>
2019-04-30 15:31:38 -0700

    fix some formatting

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/location', (request, response) => {
 app.get('/weather', (request, response) => {
   // const lat = request.query.data.latitude;
   // const lng = request.query.data.longitude;
-  
+
   try {
     const weatherData = require('./data/darksky.json');
     const weatherObj = new Weather(weatherData);
@@ -39,7 +39,6 @@ app.get('/weather', (request, response) => {
 
     response.status(500).send(errorResponse500);
   }
-
 });
 
 
