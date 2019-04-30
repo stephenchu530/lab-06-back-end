0da483ba932fe9e0835e6fa140be160e0154f4a6
STEPHEN CHU <stephen.chu530@gmail.com>
2019-04-30 10:39:25 -0700

    test: messing with server.js

--- server.js
+++ server.js
@@ -7,15 +7,20 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('./public'));
+//app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
 
 app.get('/location', (request, response) => {
-  let test = require('data/geo.json');
-  response.status(200).json('hi');
+  let res = response.send(request.query);
+  console.log(res);
+  let locationData = require('./data/geo.json');
+  if (locationData['status'] === 'OK') {
+    // let test = new LocationResponseObj(request.key1, locationData);
+  }
+  // response.status(200).json('yes');
 });
 
 app.get('/weather', (request, response) => {
@@ -26,3 +31,10 @@ app.get('/weather', (request, response) => {
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+let LocationResponseObj = function(request, jsonData) {
+  this.searchQuery = request;
+  this.formattedQuery = jsonData['results'][0]['formatted_address'];
+  this.latitude = jsonData['results'][0]['geometry']['location']['lat'];
+  this.longitude = jsonData['results'][0]['geometry']['location']['lng'];
+};
